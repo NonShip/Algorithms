1e88cf8efd0e8bfef00abbcb72e4d037f9696071
NonShip <NonShip@protroncom.com>
2021-04-19 11:34:04 +0800

    shell sort

--- Sort/Shell.hpp
+++ Sort/Shell.hpp
@@ -2,7 +2,33 @@
 #define SHELL_H
 
 
-#include "sort.h"
+#include "Sort.hpp"
+
+template<typename T>
+class Shell:public Sort<T> {
+	public:
+		void sort(vector<T> &) override;
+};
+
+template<typename T>
+void Shell<T>::sort(vector<T> &array) {
+	int h = 1, N = array.size();
+	int i, j;
+
+	while (h < N/3) {
+		h = h*3+1;
+	}
+	while (h >= 1) {
+		for (i = h; i < N; i++) {
+			for (j = i; j >= h && array[j] < array[j-h]; j-=h) {
+				swap(array[j], array[j-h]);
+			}
+		}
+		h /= 3;
+	}
+
+}
+
 
 
 
